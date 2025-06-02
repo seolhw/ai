3e57e79b84e007f0a4634529874e2c34baa95aa3
lihuiwang <seolhw@qq.com>
2025-06-02 13:09:26 +0800

    feat(api): 添加两个新的hello路由端点
    添加了/hello1和hello2两个新的GET路由端点，分别返回不同的问候消息，以扩展API功能

--- api/index.py
+++ api/index.py
@@ -4,5 +4,10 @@ app = FastAPI()
 
 
 @app.get("/")
+@app.get('/hello1')
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "World1"}
+
+@app.get('hello2')
+def read_root():
+    return {"Hello": "World2"}
