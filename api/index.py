b479b59928f9942d3544cce6248c6af897985817
lihuiwang <seolhw@qq.com>
2025-06-02 13:14:35 +0800

    feat(api): 为FastAPI应用添加前缀路径'/api'
    为API路由添加统一前缀'/api'，使所有路由路径更加规范统一

--- api/index.py
+++ api/index.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI({
+    prefix: '/api'
+})
 
 
 @app.get("/")
 @app.get('/hello1')
 def read_root():
     return {"Hello": "World1"}
+
 
 @app.get('hello2')
 def read_root2():
